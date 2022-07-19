65bf9664ecaf77d246af71c63de77a14dc6f5017
Daniel Longeuay <dannylongeuay@yahoo.com>
2022-07-18 20:44:29 -0700

    fix(*): :bug: general clean up

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides go_template language support for the [tree-sitter][] parsing library.
+//! This crate provides gotmpl language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_go_template::language()).expect("Error loading go-template grammar");
+//! parser.set_language(tree_sitter_gotmpl::language()).expect("Error loading gotmpl grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -18,14 +18,14 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_go_template() -> Language;
+    fn tree_sitter_gotmpl() -> Language;
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
 pub fn language() -> Language {
-    unsafe { tree_sitter_go_template() }
+    unsafe { tree_sitter_gotmpl() }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
@@ -47,6 +47,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading go_template language");
+            .expect("Error loading gotmpl language");
     }
 }
